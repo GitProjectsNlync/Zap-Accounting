63300bb3b6931e5a2c349697e65d1a5516f298c4
GetHub Projects Nlync <85248501+GitProjectsNlync@users.noreply.github.com>
2025-12-21 23:07:21 -0600

    Update footer.blade.php

--- resources/views/components/email/footer.blade.php
+++ resources/views/components/email/footer.blade.php
@@ -1,22 +1,24 @@
-<table border="0" style="width:100%; background-color: #F8F9FE; margin-top: 15px;">
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style="background-color:#F8F9FE; margin-top:15px;">
     <tbody>
         <tr>
-            <td>
-                <table border="0" style="text-align: center; margin: 0 auto;">
+            <td align="center">
+                <table border="0" cellpadding="0" cellspacing="0" style="margin:0 auto; text-align:center;">
                     <tbody>
                         <tr>
-                            <td align="center" valign="middle" style="text-align: center; padding-top: 5px;">
-                                <span style="color: #676ba2; text-decoration: none;">
+                            <td style="padding-top:5px; text-align:center;">
+                                <span style="color:#676ba2; font-size:12px; text-decoration:none;">
                                     Powered by Zap Accounting
                                 </span>
                             </td>
                         </tr>
+
                         <tr>
-                            <td align="center" valign="middle" style="text-align: center; padding-top: 5px;">
+                            <td style="padding-top:5px; text-align:center;">
                                 <img
                                     src="{{ asset('public/img/company.png') }}"
-                                    style="height:20px;"
+                                    height="20"
                                     alt="Zap Accounting"
+                                    style="display:block; margin:0 auto;"
                                 />
                             </td>
                         </tr>
@@ -26,8 +28,8 @@
         </tr>
 
         <tr>
-            <td style="text-align: center; padding: 5px 0; color: #595959;">
-                Zap Accounting — Simple, Powerful Accounting
+            <td style="text-align:center; padding:5px 0; color:#595959; font-size:12px;">
+                Zap Accounting &mdash; Simple, Powerful Accounting
             </td>
         </tr>
     </tbody>
